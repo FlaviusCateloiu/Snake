f0efa068d2937b1d7f86d5147a38a317f834588c
flaars <fcateloiu08@gmail.com>
2023-02-15 20:20:29 +0100

    Initial commit

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _direction = Vector2.up;
+        } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _direction = Vector2.down;
+        } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            _direction = Vector2.left;
+        } else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _direction = Vector2.right;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector3(
+            this.transform.position.x + _direction.x,
+            this.transform.position.y + _direction.y,
+            0.0f
+        );
+    }
+}
